d6b9f5bcde6242024afa203260ab729719df2752
mmg89 <67290354+mmg89@users.noreply.github.com>
2025-10-07 17:22:41 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js - Service Worker File
+const CACHE_NAME = 'apnea-monitor-cache-v1';
+const urlsToCache = [
+  './', // This will cache the main HTML file
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
